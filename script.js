70ed217c393ef33ddc1cf064a44bfdef6061d52c
notrapd <159195572+notrapd@users.noreply.github.com>
2024-04-09 21:08:57 +0000

    amazing class

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // 1. Change "cute.dog"to your name (Keep the quotation marks!)
-let username = "cute.dog";
+let username = "Wilbert";
 
 // 2. Change the number of followers from 10000 to another number.
-let followers = 10000;
+let followers = 56328191242049;
 
 
 // DO NOT CHANGE THE CODE BELOW
--- variables.js
+++ variables.js
@@ -2,17 +2,24 @@
 let favoriteColor;
 
 // 1. Create a variable called `favoriteFood`.
-
-
+let favoriteFood;
+  
 // 2. Create a variable called `favoriteNumber`.
 
-
+let favoriteNumber;
 
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
 // - Use a string value of your favorite color.
+favoriteColor = "red"
 // - Use a string value of your favorite food.
+favoriteFood = "sushi"
 // - Use a number value of your favorite number.
-
+favoriteNumber = "21"
 // 4. Console log each variable on a new line.
+console.log(favoriteColor)
+
+console.log(favoriteFood)
+
+console.log(favoriteNumber)
